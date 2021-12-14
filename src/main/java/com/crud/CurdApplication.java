0eb2b93f96631e19f2d3c96cbdf61516d135acd0
shihai.wang <wangshihai130@163.com>
2021-12-14 21:29:44 +0800

    修改扫描包

--- src/main/java/com/crud/CurdApplication.java
+++ src/main/java/com/crud/CurdApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.dzh.mapper")
+@MapperScan("com.crud.mapper")
 @SpringBootApplication
 public class CurdApplication {
 
